06dd981e708cfa22d42c009bfd2ed5ef3c51d70a
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-03-18 16:51:21 +0700

    init

---
+++ MyTodoList/Controllers/AddTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTodoViewController.swift
+//  MyTodoList
+//
+//  Created by Sendo Tjiam on 18/03/22.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyTodoList/Controllers/TodoListViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Sendo Tjiam on 18/03/22.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+    
+    private lazy var prioritySegmentedControl : UISegmentedControl = {
+        let control = UISegmentedControl(items: ["All", "Low", "Medium", "High"])
+        control.selectedSegmentIndex = 0
+        return control
+    }()
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .blue
+        return tableView
+    }()
+    private var stackView : UIStackView!
+    private lazy var floatingButton : UIButton = {
+        let btnSize : CGFloat = 50
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: btnSize, height: btnSize))
+//        btn.layer.masksToBounds = true
+        btn.backgroundColor = .systemPink
+        let image = UIImage(
+            systemName: "plus",
+            withConfiguration: UIImage.SymbolConfiguration(
+                pointSize: 28, weight: .medium)
+        )
+        btn.setImage(image, for: .normal)
+        btn.tintColor = .white
+        btn.layer.shadowRadius = 8
+        btn.layer.shadowOpacity = 0.3
+        btn.layer.cornerRadius = btnSize/2
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floatingButton.frame = CGRect(x: view.frame.width - 80, y: view.frame.height - 100, width: 50, height: 50)
+    }
+}
+
+
+extension TodoListViewController {
+    func setupUI() {
+        navigationItem.title = "Todo List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+        setupStackView()
+        setupTableView()
+        setupFloatingBtn()
+    }
+    
+    func setupStackView() {
+        stackView = UIStackView(arrangedSubviews: [prioritySegmentedControl, tableView])
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        view.addSubview(stackView)
+        setStackViewConstraints()
+    }
+    
+    func setStackViewConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
+        constraints.append(stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor))
+        constraints.append(stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func setupFloatingBtn() {
+        floatingButton.addTarget(self, action: #selector(didTapAddBtn), for: .touchUpInside)
+        view.addSubview(floatingButton)
+    }
+    
+    @objc func didTapAddBtn() {
+        DispatchQueue.main.async {
+            let vc = UINavigationController(rootViewController: AddTodoViewController())
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
+
+extension TodoListViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "teststsetset"
+        return cell
+    }
+}
